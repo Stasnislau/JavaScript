a7e4e69044f2ecc181bda0f69638606021ffbe9c
Stanislau Ryzhkou <stanislaurv@gmail.com>
2022-07-18 19:57:51 +0200

    attempt

--- Open Space/Open Space/task/src/index.js
+++ Open Space/Open Space/task/src/index.js
@@ -1,4 +1,4 @@
-const password = "TrustNo1";
+const password = "123";
 document.querySelector(".input-checkbox").addEventListener("click", function(){
     let userPassword = document.querySelector(".line").value;
     if (password === userPassword) {
@@ -11,13 +11,56 @@ const enabler = () => {
     temp.forEach(element => element.disabled = false);
     temp = document.querySelectorAll(".checkbox");
     temp.forEach(element => element.disabled = false);
-    document.querySelector(".launch-button").disabled = false;
-}
+};
 const passwordInputDisabler = () => {
     document.querySelector(".line").setAttribute("disabled","true");
     document.querySelector(".input-checkbox").setAttribute("disabled","true");
-}
-document.querySelectorAll(".checkbox").("change", function(){
-    ready++;
-})
+};
+const checkboxChecker = () => {
+    let flag = true;
+    let checkboxes = document.querySelectorAll(".checkbox");
+    checkboxes.forEach(element => {
+        if (!element.checked)
+            flag = false;
+    })
+    return flag;
+};
+const leverChecker = () => {
+    let flag = true;
+    let levers = document.querySelectorAll(".panel-levers");
+    levers.forEach(element => {
+        if (element.value !== "10") {
+            flag = false;
+        }
+    })
+    return flag;
+};
+document.querySelectorAll(".checkbox").forEach(element => element.onchange =function(){
+    if(checkboxChecker() && leverChecker()) {
+        document.querySelector(".launch-button").removeAttribute("disabled");
+    }
+});
+document.querySelectorAll(".panel-levers").forEach(element => element.onchange= function(){
+    if(checkboxChecker() && leverChecker()) {
+        document.querySelector(".launch-button").removeAttribute("disabled");
+    }
+});
+document.querySelector(".launch-button").addEventListener("click", function (){
+    let rocket = document.querySelector(".rocket");
+    rocket.animate([
+        {
+            left: '35%',
+            bottom: '40%'
+        },
+        {
+            left: '150%',
+            bottom: '150%'
+        }
+    ], {
+        duration: 2000,
+        iterations: 1
+    })
+    document.querySelector(".launch-button").setAttribute("disabled", "true");
+    setTimeout(() => {rocket.remove();},2000)
+});
 
